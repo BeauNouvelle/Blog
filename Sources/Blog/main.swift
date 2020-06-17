ddd25a4acd9d8934fadcabd993ddf2449d559047
Beau Nouvelle <getswifty@mailfence.com>
2020-06-17 15:17:10 +1000

    removed io

--- Sources/Blog/main.swift
+++ Sources/Blog/main.swift
@@ -5,6 +5,6 @@ import SplashPublishPlugin
 
 try Blog().publish(
     withTheme: .blog,
-    additionalSteps: [.deploy(using: .gitHub("BeauNouvelle/BeauNouvelle.github.io/blog"))],
+    additionalSteps: [.deploy(using: .gitHub("BeauNouvelle/blog"))],
     plugins: [.splash(withClassPrefix: "")]
 )
