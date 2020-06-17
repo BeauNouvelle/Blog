f33942a79c906b6082593da00e22a0b12abd9d65
Beau Nouvelle <getswifty@mailfence.com>
2020-06-17 15:12:36 +1000

    lowercase blog

--- Sources/Blog/main.swift
+++ Sources/Blog/main.swift
@@ -5,6 +5,6 @@ import SplashPublishPlugin
 
 try Blog().publish(
     withTheme: .blog,
-    additionalSteps: [.deploy(using: .gitHub("BeauNouvelle/BeauNouvelle.github.io/Blog"))],
+    additionalSteps: [.deploy(using: .gitHub("BeauNouvelle/BeauNouvelle.github.io/blog"))],
     plugins: [.splash(withClassPrefix: "")]
 )
